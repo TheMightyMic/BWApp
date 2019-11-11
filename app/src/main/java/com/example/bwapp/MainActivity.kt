cf4928314678f1720eec936f9934da0137c735df
mic <michisteinb@gmail.com>
2019-11-12 00:08:59 +0100

    added main buttons

--- app/src/main/java/com/example/bwapp/MainActivity.kt
+++ app/src/main/java/com/example/bwapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.bwapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val buttonClick = findViewById<TextView>(R.id.tv_click_me)
+        //buttonClick.setOnClickListener {
+        //   Toast.makeText(this@MainActivity, "Nice!", Toast.LENGTH_SHORT).show()
+        //}
     }
 }
