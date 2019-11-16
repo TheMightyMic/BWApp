a1d2145bfe8964748bcf3e7f4f163a002613d951
mic <michisteinb@gmail.com>
2019-11-16 14:25:54 +0100

    changed to bottom tab view

--- app/src/main/java/com/example/bwapp/MainActivity.kt
+++ app/src/main/java/com/example/bwapp/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.bwapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        //val buttonClick = findViewById<TextView>(R.id.tv_click_me)
-        //buttonClick.setOnClickListener {
-        //   Toast.makeText(this@MainActivity, "Nice!", Toast.LENGTH_SHORT).show()
-        //}
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
