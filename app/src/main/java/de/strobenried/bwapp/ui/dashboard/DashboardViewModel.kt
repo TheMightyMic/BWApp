964a03ea0563c187a0862f566f45b8909b89c9d2
mic <michisteinb@gmail.com>
2019-11-23 17:53:06 +0100

    text updates & added bier button

--- app/src/main/java/de/strobenried/bwapp/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/de/strobenried/bwapp/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Do kena d Leid schaun wer im bw is und wos a soiba gsuffa hod."
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/de/strobenried/bwapp/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/de/strobenried/bwapp/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Do kimmt irgendwos anders her"
     }
     val text: LiveData<String> = _text
 }
