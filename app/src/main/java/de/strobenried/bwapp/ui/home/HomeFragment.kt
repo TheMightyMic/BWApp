356972ed345570167a0021d353fa894873b40112
mic <michisteinb@gmail.com>
2019-11-26 15:34:58 +0100

    added button_beer onClickListener

--- app/src/main/java/de/strobenried/bwapp/ui/home/HomeFragment.kt
+++ app/src/main/java/de/strobenried/bwapp/ui/home/HomeFragment.kt
@@ -4,21 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 import de.strobenried.bwapp.R
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(), View.OnClickListener {
 
     private lateinit var homeViewModel: HomeViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    var database = FirebaseFirestore.getInstance()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
@@ -26,6 +28,22 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        val beerBtn: Button = root.findViewById(R.id.button_beer)
+        beerBtn.setOnClickListener(this)
+
         return root
     }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button_beer -> {
+
+            }
+
+            else -> {
+            }
+        }
+    }
+
 }
