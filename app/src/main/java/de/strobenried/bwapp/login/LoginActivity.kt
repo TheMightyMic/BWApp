5411ca3358b8ddf98adb8ba46938854a91fb251f
mic <michisteinb@gmail.com>
2019-11-26 14:30:10 +0100

    Login information gets now saved  in sharedPrefs

--- app/src/main/java/de/strobenried/bwapp/login/LoginActivity.kt
+++ app/src/main/java/de/strobenried/bwapp/login/LoginActivity.kt
@@ -1,14 +1,15 @@
 package de.strobenried.bwapp.login
 
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import de.strobenried.bwapp.MainActivity
@@ -41,23 +42,16 @@ class LoginActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
+        this.loadLoginInformation()
+
         loginBtn.setOnClickListener {
-            var email: String = emailEt.text.toString()
-            var password: String = passwordEt.text.toString()
+            val email: String = emailEt.text.toString()
+            val password: String = passwordEt.text.toString()
 
             if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
                 Toast.makeText(this@LoginActivity, "Please fill all the fields", Toast.LENGTH_LONG).show()
             } else{
-                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
-                    if(task.isSuccessful) {
-                        Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_LONG).show()
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }else {
-                        Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
-                    }
-                })
+                loginToDb(email,password)
             }
         }
 
@@ -70,6 +64,38 @@ class LoginActivity : AppCompatActivity() {
         resetPasswordTv.setOnClickListener{
             val intent = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(intent)
+        }
+    }
+    private fun loginToDb(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
+            if(task.isSuccessful) {
+                Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_LONG).show()
+                saveAuthValues(email,password)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else {
+                Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    private fun saveAuthValues(email: String, password: String) {
+        val sharedPrefs = getSharedPreferences("Login", Context.MODE_PRIVATE).edit()
+        sharedPrefs.putString("Email", email)
+        sharedPrefs.putString("Password", password)
+        sharedPrefs.apply()
+    }
+
+    private fun loadLoginInformation() {
+        val sp1 = getSharedPreferences("Login", Context.MODE_PRIVATE)
+
+        val email = sp1.getString("Email", null)
+        val password = sp1.getString("Password", null)
+
+        if(email.isNullOrEmpty() || password.isNullOrEmpty()) { return }
+        else{
+            loginToDb(email,password)
         }
     }
 }
